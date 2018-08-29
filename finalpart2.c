92798daa9b04afc8a6a7db3f90d0aa3228c20173
AnthonyValverde <valverde.anthony97@gmail.com>
2018-08-28 21:48:13 -0700

    Update finalpart2.c

--- finalpart2.c
+++ finalpart2.c
@@ -25,6 +25,13 @@ unsigned short player1p = 0;
 unsigned short player2p = 0;
 unsigned short i;
 unsigned short direction = 0;
+unsigned char column_val1 = 0x01; // sets the pattern displayed on columns
+unsigned char column_sel1 = 0x7F; // grounds column to display pattern
+unsigned char column_val2 = 0x01; // sets the pattern displayed on columns
+unsigned char column_sel2 = 0x7F; // grounds column to display pattern
+unsigned char column_val3 = 0x01; // sets the pattern displayed on columns
+unsigned char column_sel3 = 0x7F; // grounds column to display pattern
+
 void TimerOn() {
 	// AVR timer/counter controller register TCCR1
 	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
@@ -79,6 +86,7 @@ void TimerSet(unsigned long M) {
 // SM1: DEMO LED matrix
 // ====================
 enum SM1_States {sm1_display};
+	
 int SM1_Tick(int state) {
 
 	// === Local Variables ===
@@ -112,427 +120,428 @@ int SM1_Tick(int state) {
 		default:   	        break;
 	}
 	
-	PORTA = column_val; // PORTA displays column pattern
-	PORTB = column_sel; // PORTB selects column to display pattern
+	PORTB = column_val; // PORTA displays column pattern
+	PORTA = column_sel; // PORTB selects column to display pattern
 
 	return state;
-};
-void physics () 
+}; 
+void physics ()
 {
-	switch(SM_STATE3) 
+	switch(SM_STATE3)
 	{
 		case SM_SRIGHT:
-			column_sel3 = !(column_sel3);
-			column_sel3 = column_sel3 >> 1 ;
-			column_sel3 = !(column_sel3);
+		column_sel3 = !(column_sel3);
+		column_sel3 = column_sel3 >> 1 ;
+		column_sel3 = !(column_sel3);
 		break;
 		case SM_SLEFT:
-			column_sel3 = !(column_sel3);
-			column_sel3 = column_sel3 << 1 ;
-			column_sel3 = !(column_sel3);
+		column_sel3 = !(column_sel3);
+		column_sel3 = column_sel3 << 1 ;
+		column_sel3 = !(column_sel3);
 		break;
 		case SM_DOWNRIGHT:
-			column_sel3 = !(column_sel3);
-			column_sel3 = column_sel3 >> 1 ;
-			column_sel3 = !(column_sel3);
-			column_val3 = column val3 >> 1;
+		column_sel3 = !(column_sel3);
+		column_sel3 = column_sel3 >> 1 ;
+		column_sel3 = !(column_sel3);
+		column_val3 = column_val3 >> 1;
 		break;
 		case SM_DOWNLEFT:
-			column_sel3 = !(column_sel3);
-			column_sel3 = column_sel3 << 1 ;
-			column_sel3 = !(column_sel3);
-			column_val3 = column val3 << 1;
+		column_sel3 = !(column_sel3);
+		column_sel3 = column_sel3 << 1 ;
+		column_sel3 = !(column_sel3);
+		column_val3 = column_val3 << 1;
 		break;
 		case SM_UPRIGHT:
-			column_sel3 = !(column_sel3);
-			column_sel3 = column_sel3 >> 1 ;
-			column_sel3 = !(column_sel3);
-			column_val3 = column val3 >> 1;
+		column_sel3 = !(column_sel3);
+		column_sel3 = column_sel3 >> 1 ;
+		column_sel3 = !(column_sel3);
+		column_val3 = column_val3 >> 1;
 		break;
 		case SM_UPLEFT:
-			column_sel3 = !(column_sel3);
-			column_sel3 = column_sel3 << 1 ;
-			column_sel3 = !(column_sel3);
-			column_val3 = column val3 << 1;
+		column_sel3 = !(column_sel3);
+		column_sel3 = column_sel3 << 1 ;
+		column_sel3 = !(column_sel3);
+		column_val3 = column_val3 << 1;
 		break;
 	}
-void SM_TICK()
-{
-	switch(SM_STATE)
+}
+	void SM_TICK()
 	{
-		case SM_ZERO:
-		if ((button1 == 0x00) && (button2 == 0x00))
+		switch(SM_STATE)
 		{
-			SM_STATE = SM_ZERO;
-		}
-		else if ((button1 == 0x01) && (button2 == 0x00))
-		{
-			SM_STATE = SM_INCREMENT;
-		}
-		else if ((button1 == 0x00) && (button2 == 0x01))
-		{
-			SM_STATE = SM_DECREMENT;
-		}
-		else if ((button1 == 0x01) && (button2 == 0x01))
-		{
-			SM_STATE = SM_ZERO;
-		}
-		break;
-		
-		case SM_INCREMENT:
-		if ((lightup < 6) && (cnt <= 0))
-		{
-			lightup = lightup + 1;
-		}
-		
-		if ((button1 == 0x00) && (button2 == 0x00))
-		{
-			SM_STATE = SM_ZERO;
-			cnt = 0;
-		}
-		else if ((button1 == 0x01) && (button2 == 0x00))
-		{
-			cnt = cnt +1;
-			if (cnt >= 5)
+			case SM_ZERO:
+			if ((button1 == 0x00) && (button2 == 0x00))
 			{
+				SM_STATE = SM_ZERO;
+			}
+			else if ((button1 == 0x01) && (button2 == 0x00))
+			{
+				SM_STATE = SM_INCREMENT;
+			}
+			else if ((button1 == 0x00) && (button2 == 0x01))
+			{
+				SM_STATE = SM_DECREMENT;
+			}
+			else if ((button1 == 0x01) && (button2 == 0x01))
+			{
+				SM_STATE = SM_ZERO;
+			}
+			break;
+			
+			case SM_INCREMENT:
+			if ((lightup < 6) && (cnt <= 0))
+			{
+				lightup = lightup + 1;
+			}
+			
+			if ((button1 == 0x00) && (button2 == 0x00))
+			{
+				SM_STATE = SM_ZERO;
 				cnt = 0;
 			}
-			SM_STATE = SM_INCREMENT;
-			
-			
-		}
-		else if ((button1 == 0x00) && (button2 == 0x01))
-		{
-			SM_STATE = SM_DECREMENT;
-			cnt = 0;
-		}
-		else if ((button1 == 0x01) && (button2 == 0x01))
-		{
-			SM_STATE = SM_ZERO;
-			cnt = 0;
-		}
-		break;
-		
-		case SM_DECREMENT:
-		if ((lightup > 1) && (cnt <= 0))
-		{
-			lightup = lightup - 1;
-		}
-
-		if ((button1 == 0x00) && (button2 == 0x00))
-		{
-			SM_STATE = SM_ZERO;
-			cnt = 0;
-		}
-		else if ((button1 == 0x01) && (button2 == 0x00))
-		{
-			SM_STATE = SM_INCREMENT;
-			cnt = 0;
-		}
-		else if ((button1 == 0x00) && (button2 == 0x01))
-		{
-			SM_STATE = SM_DECREMENT;
-			cnt = cnt + 1;
-			if (cnt >= 5)
+			else if ((button1 == 0x01) && (button2 == 0x00))
 			{
+				cnt = cnt +1;
+				if (cnt >= 5)
+				{
+					cnt = 0;
+				}
+				SM_STATE = SM_INCREMENT;
+				
+				
+			}
+			else if ((button1 == 0x00) && (button2 == 0x01))
+			{
+				SM_STATE = SM_DECREMENT;
 				cnt = 0;
 			}
-		}
-		
-		else if ((button1 == 0x01) && (button2 == 0x01))
-		{
-			SM_STATE = SM_ZERO;
-			cnt = 0;
-		}
-		break;
-	}
-}
-
-void SM_TICK2()
-{
-	switch(SM_STATE2)
-	{
-		case SM2_ZERO:
-		if ((button3 == 0x00) && (button4 == 0x00))
-		{
-			SM_STATE2 = SM2_ZERO;
-		}
-		else if ((button3 == 0x01) && (button4 == 0x00))
-		{
-			SM_STATE2 = SM2_INCREMENT;
-		}
-		else if ((button3 == 0x00) && (button4 == 0x01))
-		{
-			SM_STATE2 = SM2_DECREMENT;
-		}
-		else if ((button3 == 0x01) && (button4 == 0x01))
-		{
-			SM_STATE2 = SM2_ZERO;
-		}
-		break;
-		
-		case SM2_INCREMENT:
-		if ((lightup1 < 6) && (cnt1 = 0))
-		{
-			lightup1 = lightup1 + 1;
-		}
-		
-		if ((button3 == 0x00) && (button4 == 0x00))
-		{
-			SM_STATE2 = SM2_ZERO;
-			cnt1 = 0;
-		}
-		else if ((button3 == 0x01) && (button4 == 0x00))
-		{
-			cnt1 = cnt1 +1;
-			if (cnt1 >= 5)
+			else if ((button1 == 0x01) && (button2 == 0x01))
 			{
+				SM_STATE = SM_ZERO;
+				cnt = 0;
+			}
+			break;
+			
+			case SM_DECREMENT:
+			if ((lightup > 1) && (cnt <= 0))
+			{
+				lightup = lightup - 1;
+			}
+
+			if ((button1 == 0x00) && (button2 == 0x00))
+			{
+				SM_STATE = SM_ZERO;
+				cnt = 0;
+			}
+			else if ((button1 == 0x01) && (button2 == 0x00))
+			{
+				SM_STATE = SM_INCREMENT;
+				cnt = 0;
+			}
+			else if ((button1 == 0x00) && (button2 == 0x01))
+			{
+				SM_STATE = SM_DECREMENT;
+				cnt = cnt + 1;
+				if (cnt >= 5)
+				{
+					cnt = 0;
+				}
+			}
+			
+			else if ((button1 == 0x01) && (button2 == 0x01))
+			{
+				SM_STATE = SM_ZERO;
+				cnt = 0;
+			}
+			break;
+		}
+	}
+
+	void SM_TICK2()
+	{
+		switch(SM_STATE2)
+		{
+			case SM2_ZERO:
+			if ((button3 == 0x00) && (button4 == 0x00))
+			{
+				SM_STATE2 = SM2_ZERO;
+			}
+			else if ((button3 == 0x01) && (button4 == 0x00))
+			{
+				SM_STATE2 = SM2_INCREMENT;
+			}
+			else if ((button3 == 0x00) && (button4 == 0x01))
+			{
+				SM_STATE2 = SM2_DECREMENT;
+			}
+			else if ((button3 == 0x01) && (button4 == 0x01))
+			{
+				SM_STATE2 = SM2_ZERO;
+			}
+			break;
+			
+			case SM2_INCREMENT:
+			if ((lightup1 < 6) && (cnt1 == 0))
+			{
+				lightup1 = lightup1 + 1;
+			}
+			
+			if ((button3 == 0x00) && (button4 == 0x00))
+			{
+				SM_STATE2 = SM2_ZERO;
 				cnt1 = 0;
 			}
-			SM_STATE2 = SM2_INCREMENT;
-			
-			
-		}
-		else if ((button3 == 0x00) && (button4 == 0x01))
-		{
-			SM_STATE2 = SM2_DECREMENT;
-			cnt1= 0;
-		}
-		else if ((button3 == 0x01) && (button4 == 0x01))
-		{
-			SM_STATE2 = SM2_ZERO;
-			cnt1 = 0;
-		}
-		break;
-		
-		case SM2_DECREMENT:
-		if ((lightup1 > 1) && (cnt1 <= 0))
-		{
-			lightup1 = lightup1 - 1;
-		}
-
-		if ((button3 == 0x00) && (button4 == 0x00))
-		{
-			SM_STATE2 = SM2_ZERO;
-			cnt1 = 0;
-		}
-		else if ((button3 == 0x01) && (button4 == 0x00))
-		{
-			SM_STATE2 = SM2_INCREMENT;
-			cnt1 = 0;
-		}
-		else if ((button3 == 0x00) && (button4 == 0x01))
-		{
-			SM_STATE2 = SM2_DECREMENT;
-			cnt1 = cnt1 + 1;
-			if (cnt1 >= 5)
+			else if ((button3 == 0x01) && (button4 == 0x00))
 			{
+				cnt1 = cnt1 +1;
+				if (cnt1 >= 5)
+				{
+					cnt1 = 0;
+				}
+				SM_STATE2 = SM2_INCREMENT;
+				
+				
+			}
+			else if ((button3 == 0x00) && (button4 == 0x01))
+			{
+				SM_STATE2 = SM2_DECREMENT;
+				cnt1= 0;
+			}
+			else if ((button3 == 0x01) && (button4 == 0x01))
+			{
+				SM_STATE2 = SM2_ZERO;
 				cnt1 = 0;
 			}
+			break;
+			
+			case SM2_DECREMENT:
+			if ((lightup1 > 1) && (cnt1 <= 0))
+			{
+				lightup1 = lightup1 - 1;
+			}
+
+			if ((button3 == 0x00) && (button4 == 0x00))
+			{
+				SM_STATE2 = SM2_ZERO;
+				cnt1 = 0;
+			}
+			else if ((button3 == 0x01) && (button4 == 0x00))
+			{
+				SM_STATE2 = SM2_INCREMENT;
+				cnt1 = 0;
+			}
+			else if ((button3 == 0x00) && (button4 == 0x01))
+			{
+				SM_STATE2 = SM2_DECREMENT;
+				cnt1 = cnt1 + 1;
+				if (cnt1 >= 5)
+				{
+					cnt1 = 0;
+				}
+			}
+			
+			else if ((button3 == 0x01) && (button4 == 0x01))
+			{
+				SM_STATE2 = SM2_ZERO;
+				cnt1 = 0;
+			}
+			break;
 		}
-		
-		else if ((button3 == 0x01) && (button4 == 0x01))
-		{
-			SM_STATE2 = SM2_ZERO;
-			cnt1 = 0;
-		}
-		break;
-	}
-}
-void intromessage()
-{	for(unsigned short i = 0;  i <= 10; ++i)
-	{
-		LCD_DisplayString(1, "Welcome Ping    Pong Playa!!!");
-		while (!TimerFlag);
-		TimerFlag = 0;
-	}
-	while((button1 == 0x00) && (button2 == 0x00) && (button3 == 0x00) && (button4 == 0x00))
-	{	button1 = !(PIND & 0x01);
-		button2 = !(PIND & 0x02);
-		button3 = !(PIND & 0x04);
-		button4 = !(PIND & 0x08);
-		LCD_DisplayString(1, "Press any key");
-	}
-}
-void paddles()
-{
-	if (lightup == 1)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel1 = 0x7F;
-			column_val1 = 0x01;
-			PORTA = column_val1;
-			PORTB = column_sel1;
-			column_val1 = column_val1 << 1;
-		}
-	}
-	else if (lightup == 2)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel1 = 0x7F;
-			column_val1 = 0x02;
-			PORTA = column_val1;
-			PORTB = column_sel1;
-			column_val1 = column_val1 << 1;
-		}
-	}
-	else if (lightup == 3)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel = 0x7F;
-			column_val = 0x04;
-			PORTA = column_val1;
-			PORTB = column_sel1;
-			column_val = column_val << 1;
-		}
-	}
-	else if (lightup == 4)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel = 0x7F;
-			column_val = 0x08;
-			PORTA = column_val1;
-			PORTB = column_sel1;
-			column_val = column_val << 1;
-		}
-	}
-	else if (lightup == 5)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel = 0x7F;
-			column_val = 0x10;
-			PORTA = column_val1;
-			PORTB = column_sel1;
-			column_val = column_val << 1;
-		}
-	}
-	else
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel = 0x7F;
-			column_val = 0x20;
-			PORTA = column_val1;
-			PORTB = column_sel1;
-			column_val = column_val << 1;
-		}
-	}
-}
-void paddles2()
-{
-	if (lightup1 == 1)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel2 = 0xFE;
-			column_val2 = 0x01;
-			PORTA = column_val2;
-			PORTB = column_sel2;
-			column_val2 = column_val2 << 1;
-		}	
-	}
-	else if (lightup1 == 2)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel2 = 0xFE;
-			column_val2 = 0x02;
-			PORTA = column_val2;
-			PORTB = column_sel2;
-			column_val2 = column_val2 << 1;
-		}
-	}
-	else if (lightup1 == 3)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel2 = 0xFE;
-			column_val2 = 0x04;
-			PORTA = column_val2;
-			PORTB = column_sel2;
-			column_val2 = column_val2 << 1;
-		}
-	}
-	else if (lightup1 == 4)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel2 = 0xFE;
-			column_val2 = 0x08;
-			PORTA = column_val2;
-			PORTB = column_sel2;
-			column_val2 = column_val2 << 1;
-		}
-	}
-	else if (lightup1 == 5)
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel2 = 0xFE;
-			column_val2 = 0x10;
-			PORTA = column_val1;
-			PORTB = column_sel2;
-			column_val2 = column_val2 << 1;
-		}
-	}
-	else
-	{
-		for(i = 0; i < 3; ++i)
-		{
-			column_sel2 = 0xFE;
-			column_val2 = 0x20;
-			PORTA = column_val1;
-			PORTB = column_sel2;
-			column_val2 = column_val2 << 1;
-		}
-	}
-}
-void checkrows () {
 	
-}
-void main()
-{	DDRA = 0xFF; PORTA = 0x00;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0xFF; PORTC = 0x00;
-	DDRD = 0xF0; PORTD = 0x0F;
-	TimerSet(150);
-	TimerOn();
-	LCD_init();
-	SM_STATE = SM_ZERO;
-	SM_STATE2 = SM2_ZERO;
- 	SM_STATE3 - SM_SRIGHT
-	lightup = 3;
-	lightup1 = 3;
- 	direction = 1;
-	intromessage();
-	while(1) {
-		//button input to move paddles
-		button1 = !(PIND & 0x01);
-		button2 = !(PIND & 0x02);
-		button3 = !(PIND & 0x04);
-		button4 = !(PIND & 0x08);
-		
-		//LED MATRIX GAMEPLAY
-		SM_TICK(); // if button pressed moved paddle1
-		SM_TICK2(); // if button pressed move paddle2
-		SM1_Tick(1);
-		paddles(); // move paddle 1
-		paddles2(); // move paddle 2
-		
-		//display points
-		LCD_DisplayString(1, "Player 1: ");
-		LCD_Cursor(11);
-		LCD_WriteData('0' + player1p);
-		LCD_DisplayString(17, "Player 2: ");
-		LCD_Cursor(28);
-		LCD_WriteData('0' + player2p);
-		while (!TimerFlag);
-		TimerFlag = 0;
-
 	}
-}
+	void intromessage()
+	{	for(unsigned short i = 0;  i <= 10; ++i)
+		{
+			LCD_DisplayString(1, "Welcome Ping    Pong Playa!!!");
+			while (!TimerFlag);
+			TimerFlag = 0;
+		}
+		while((button1 == 0x00) && (button2 == 0x00) && (button3 == 0x00) && (button4 == 0x00))
+		{	button1 = !(PIND & 0x01);
+			button2 = !(PIND & 0x02);
+			button3 = !(PIND & 0x04);
+			button4 = !(PIND & 0x08);
+			LCD_DisplayString(1, "Press any key");
+		}
+	}
+	void paddles()
+	{
+		if (lightup == 1)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel1 = 0x7F;
+				column_val1 = 0x01;
+				PORTB = column_val1;
+				PORTA = column_sel1;
+				column_val1 = column_val1 << 1;
+			}
+		}
+		else if (lightup == 2)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel1 = 0x7F;
+				column_val1 = 0x02;
+				PORTB = column_val1;
+				PORTA = column_sel1;
+				column_val1 = column_val1 << 1;
+			}
+		}
+		else if (lightup == 3)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel1 = 0x7F;
+				column_val1 = 0x04;
+				PORTB = column_val1;
+				PORTA = column_sel1;
+				column_val1 = column_val1 << 1;
+			}
+		}
+		else if (lightup == 4)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel1 = 0x7F;
+				column_val1 = 0x08;
+				PORTB = column_val1;
+				PORTA = column_sel1;
+				column_val1 = column_val1 << 1;
+			}
+		}
+		else if (lightup == 5)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel1 = 0x7F;
+				column_val1 = 0x10;
+				PORTB = column_val1;
+				PORTA = column_sel1;
+				column_val1 = column_val1 << 1;
+			}
+		}
+		else
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel1 = 0x7F;
+				column_val1 = 0x20;
+				PORTB = column_val1;
+				PORTA = column_sel1;
+				column_val1 = column_val1 << 1;
+			}
+		}
+	}
+	void paddles2()
+	{
+		if (lightup1 == 1)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel2 = 0xFE;
+				column_val2 = 0x01;
+				PORTB = column_val2;
+				PORTA = column_sel2;
+				column_val2 = column_val2 << 1;
+			}
+		}
+		else if (lightup1 == 2)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel2 = 0xFE;
+				column_val2 = 0x02;
+				PORTB = column_val2;
+				PORTA = column_sel2;
+				column_val2 = column_val2 << 1;
+			}
+		}
+		else if (lightup1 == 3)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel2 = 0xFE;
+				column_val2 = 0x04;
+				PORTB = column_val2;
+				PORTA = column_sel2;
+				column_val2 = column_val2 << 1;
+			}
+		}
+		else if (lightup1 == 4)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel2 = 0xFE;
+				column_val2 = 0x08;
+				PORTB = column_val2;
+				PORTA = column_sel2;
+				column_val2 = column_val2 << 1;
+			}
+		}
+		else if (lightup1 == 5)
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel2 = 0xFE;
+				column_val2 = 0x10;
+				PORTB = column_val2;
+				PORTA = column_sel2;
+				column_val2 = column_val2 << 1;
+			}
+		}
+		else
+		{
+			for(i = 0; i < 3; ++i)
+			{
+				column_sel2 = 0xFE;
+				column_val2 = 0x20;
+				PORTB = column_val2;
+				PORTA = column_sel2;
+				column_val2 = column_val2 << 1;
+			}
+		}
+	} 
+	void checkrows () {
+		
+	}
+	void main()
+	{	DDRA = 0xFF; PORTA = 0x00;
+		DDRB = 0xFF; PORTB = 0x00;
+		DDRC = 0xFF; PORTC = 0x00;
+		DDRD = 0xF0; PORTD = 0x0F;
+		TimerSet(150);
+		TimerOn();
+		LCD_init();
+		SM_STATE = SM_ZERO;
+		SM_STATE2 = SM2_ZERO;
+		//SM_STATE3 - SM_SRIGHT;
+		lightup = 3;
+		lightup1 = 3;
+		direction = 1;
+		//intromessage();
+		while(1) {
+			//button input to move paddles
+			button1 = !(PIND & 0x01);
+			button2 = !(PIND & 0x02);
+			button3 = !(PIND & 0x04);
+			button4 = !(PIND & 0x08);
+			
+			//LED MATRIX GAMEPLAY
+			SM_TICK(); // if button pressed moved paddle1
+			SM_TICK2(); // if button pressed move paddle2
+			paddles(); // move paddle 1
+			paddles2(); // move paddle 2
+			
+			//display points
+			LCD_DisplayString(1, "Player 1: ");
+			LCD_Cursor(11);
+			LCD_WriteData('0' + player1p);
+			LCD_DisplayString(17, "Player 2: ");
+			LCD_Cursor(28);
+			LCD_WriteData('0' + player2p);
+			while (!TimerFlag);
+			TimerFlag = 0;
+
+		}
+	}
